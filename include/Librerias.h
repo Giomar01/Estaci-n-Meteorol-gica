9bb388c10058110ac4305b598a95279663b0df48
GiomarMoreno <giomarmoreno01@gmail.com>
2026-06-03 17:57:42 -0500

    commit creacion archivo librerias.h

---
+++ include/Librerias.h
@@ -0,0 +1,18 @@
+#ifndef LIBRERIAS_H
+
+// Se definen las librerias y elementos globales para llevar a cabo el desarrollo del proyecto
+
+#define LIBRERIAS_H
+
+// Declaraciones de librerias y elementos globales para el proyecto
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+// Uso de espacios de nombres para facilitar la escritura del código profe :3
+using std::string;
+using std::vector;
+
+
+#endif
