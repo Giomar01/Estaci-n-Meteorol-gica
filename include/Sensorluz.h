592d4c51238e3670847a2b519ee8a88f8f22c87a
GiomarMoreno <giomarmoreno01@gmail.com>
2026-06-03 20:20:43 -0500

    commit Sensorluz.h final

--- include/Sensorluz.h
+++ include/Sensorluz.h
@@ -10,3 +10,34 @@
 class SensorLuz : public Sensor {
 private:
     float porcentajeLuz;
+    public:
+    /**
+     * @brief Constructor para SensorLuz.
+     * @param pin Pin analógico de conexión.
+     */
+    SensorLuz(int pin);
+
+    /**
+     * @brief Destructor de SensorLuz.
+     */
+    ~SensorLuz() override;
+
+    /**
+     * @brief Obtiene el porcentaje de luz actual.
+     * @return Valor flotante entre 0.0 y 100.0.
+     */
+    float getPorcentajeLuz() const;
+
+    /**
+     * @brief Establece el porcentaje de luz.
+     * @param luz Porcentaje de luz.
+     */
+    void setPorcentajeLuz(float luz);
+
+    /**
+     * @brief Lee el valor analógico y lo convierte a porcentaje de luz[cite: 124].
+     */
+    void leerDatos() override;
+};
+
+#endif
