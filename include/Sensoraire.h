895f9c967f3c7a2b65fbe43f07eb1fdd2c9f541c
GiomarMoreno <giomarmoreno01@gmail.com>
2026-06-03 20:26:56 -0500

    commit Sensoraire.h final

--- include/Sensoraire.h
+++ include/Sensoraire.h
@@ -16,3 +16,27 @@ public:
      * @param pin Pin analógico de lectura.
      */
     SensorAire(int pin);
+     /**
+     * @brief Destructor de SensorAire.
+     */
+    ~SensorAire() override;
+
+    /**
+     * @brief Obtiene la concentración de gas medida en ppm.
+     * @return Valor en partes por millón.
+     */
+    float getPpmCalidad() const;
+
+    /**
+     * @brief Establece las partes por millón de la calidad del aire.
+     * @param ppm Valor en partes por millón.
+     */
+    void setPpmCalidad(float ppm);
+
+    /**
+     * @brief Lee el sensor y calcula la presencia de gases en el aire.
+     */
+    void leerDatos() override;
+};
+
+#endif
